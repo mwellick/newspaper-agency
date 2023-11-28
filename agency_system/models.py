6e08e1b6c3039117d50b110cc0c0ff6ac9f13d1c
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-11-28 20:57:32 +0200

    Created models according to the architecture of project,made migrate …

--- agency_system/models.py
+++ agency_system/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic, on_delete=models.CASCADE, related_name="newspapers"
+    )
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="published_newspapers"
+    )
