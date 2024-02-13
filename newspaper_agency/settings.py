6b14695e4a56b052631c7b111957db67ae59ced2
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-02-13 13:25:07 +0200

    Updated settings and added requirements.txt

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -19,15 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=s8rb*5ybi8w+j8j^@v%#zpzc4movh&sakijvql=un$!33z=6y"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-=s8rb*5ybi8w+j8j^@v%#zpzc4movh&sakijvql=un$!33z=6y")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-if DEBUG:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
