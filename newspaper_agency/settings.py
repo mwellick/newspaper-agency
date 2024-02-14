cb5a8015ec9473f835f7178a820fb5742743dd9f
Mykhailo Korotia <mr.anderson528491@gmail.com>
2024-02-14 21:50:40 +0200

    Updated setting.py

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-ALLOWED_HOSTS = ["127.0.0.1","https://newspaper-agency-e2et.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
